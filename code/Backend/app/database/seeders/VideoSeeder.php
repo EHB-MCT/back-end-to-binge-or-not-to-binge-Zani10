982767bc1105eadb1660daab16432431b8aad43e
Zani Dobruna <zani.dobruna@student.ehb.be>
2024-06-24 17:12:11 +0200

    Solved YouTube embedding issue

--- code/Backend/app/database/seeders/VideoSeeder.php
+++ code/Backend/app/database/seeders/VideoSeeder.php
@@ -17,23 +17,22 @@ class VideoSeeder extends Seeder
         Video::create([
             'title' => 'How to Build a Bookshelf',
             'description' => 'Step-by-step guide to building a bookshelf.',
-            'url' => 'https://youtu.be/t4kcQtlIcxU',
-            'category_id' => $woodworking->id
+            'url' => 'https://www.youtube.com/embed/t4kcQtlIcxU',
+            'category_id' => $woodworking->id,
         ]);
 
         Video::create([
             'title' => 'Painting a Room: Tips and Tricks',
             'description' => 'Learn the best techniques for painting a room.',
-            'url' => 'https://youtu.be/iCIZTXkIweg',
-            'category_id' => $painting->id
+            'url' => 'https://www.youtube.com/embed/iCIZTXkIweg',
+            'category_id' => $painting->id,
         ]);
 
         Video::create([
             'title' => 'DIY Garden Path',
             'description' => 'Create a beautiful garden path with these easy steps.',
-            'url' => 'https://youtu.be/6vKq6J-Mxn8',
-            'category_id' => $gardening->id
+            'url' => 'https://www.youtube.com/embed/6vKq6J-Mxn8',
+            'category_id' => $gardening->id,
         ]);
     }
 }
-

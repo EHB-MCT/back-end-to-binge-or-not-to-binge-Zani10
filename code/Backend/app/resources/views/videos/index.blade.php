1cbf2ce03462af3ed7bfd6a76bfe1f42e4a0af92
Zani Dobruna <zani.dobruna@student.ehb.be>
2024-06-24 17:13:12 +0200

    Added Bootstrap styling

--- code/Backend/app/resources/views/videos/index.blade.php
+++ code/Backend/app/resources/views/videos/index.blade.php
@@ -3,11 +3,18 @@
 @section('content')
     <div class="container">
         <h1>DIY Videos</h1>
-        @foreach($videos as $video)
-            <div>
-                <h2><a href="{{ route('videos.show', $video->id) }}">{{ $video->title }}</a></h2>
-                <p>{{ $video->description }}</p>
-            </div>
-        @endforeach
+        <div class="row">
+            @foreach($videos as $video)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $video->title }}</h5>
+                            <p class="card-text">{{ $video->description }}</p>
+                            <a href="{{ route('videos.show', $video->id) }}" class="btn btn-primary">Watch Video</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection

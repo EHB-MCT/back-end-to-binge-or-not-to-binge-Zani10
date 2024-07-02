9291065d99c32e346bf75b86013a83f682e26538
Zani Dobruna <zani.dobruna@student.ehb.be>
2024-07-02 08:34:22 +0300

    Home route fixed after logging in

--- code/Backend/app/routes/web.php
+++ code/Backend/app/routes/web.php
@@ -11,9 +11,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [VideoController::class, 'index'])->name('home');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::get('/home', function () {
+    return redirect()->route('videos.index');
+});
 
 Auth::routes();
 
